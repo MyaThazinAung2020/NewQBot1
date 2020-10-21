049563815b813517c9833742268e32a369bc28be
Mya Thazin Aung <myathazin.aung@alfaconnections.com>
2020-10-21 13:40:14 +0630

    Add project files.

---
+++ QuestionsTabApp/src/environments/environment.ts
@@ -0,0 +1,25 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: "https://mtzaqbot.azurewebsites.net/api/Request/",
+    selfUrl: "https://mtzaqbot-questions.azurewebsites.net",
+
+    authConfig: {
+        instance: "https://login.microsoftonline.com/",
+        tenantId: "aa81ea33-cf73-444b-b305-006b188a8fb4",
+        clientId: "83ab00f9-0dd5-4da0-a57b-5deaaf6ffba8",
+        redirectUri: "/app-silent-end",
+        cacheLocation: "localStorage",
+        navigateToLoginRequestUrl: false,
+        extraQueryParameters: "",
+        popUp: true,
+        popUpUri: "/app-silent-start",
+        popUpWidth: 600,
+        popUpHeight: 535
+    },
+
+    // do not populate the following:
+    upn: "",
+    tid: "",
+    gid: "",
+    cname: ""
+};
